0ab7d24a5c50cb46419975cef08a411786edb820
Dindon2087 <120275633+Dindon2087@users.noreply.github.com>
2023-02-18 11:02:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// static void Main()
+//         {
+//             double sum=0, avg=0;
+//             double[] numbers = { 10, 20, 50, 40};
+//             for(int i=0;i<numbers.Length;i++)
+//             {
+//                 sum += numbers[i];
+//             }
+//             avg = sum / numbers.Length;
+//             Console.WriteLine("The Sum is : "+sum);
+//             Console.WriteLine("The Average is : "+avg);
+            
+//             Console.ReadKey();
+//         }
+
+// Задача. Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. 
+//Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+ /*string[] array1 = new string[5] {"689", "34", "hello", "people", "res"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);*/
